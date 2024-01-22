5a1cc2f0bac04c1ba2967096045ccf749eb47c21
EC2 Default User <ec2-user@ip-172-31-38-8.eu-central-1.compute.internal>
2024-01-22 07:56:46 +0000

    2201

--- ui_bot/urls.py
+++ ui_bot/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include, re_path
 from app.views import index,save,gtp,detail,create_detail
 from django.conf import settings
 from django.conf.urls.static import static
@@ -43,6 +43,6 @@ urlpatterns = [
         name="swagger-ui",
     ),
 ]
-if settings.DEBUG:
+if True:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
