e05e23f6f57e5a1bc48b63f5a92758e1d741a4c5
EC2 Default User <ec2-user@ip-172-31-21-89.eu-central-1.compute.internal>
2024-01-24 09:32:56 +0000

    model changed

--- logs/admin.py
+++ logs/admin.py
@@ -3,7 +3,7 @@ from logs.models import Log
 
 @admin.register(Log)
 class LogAdmin(admin.ModelAdmin):
-    list_display = ('question', 'answer', 'channel', 'token', 'user_id', 'is_error_occurred', 'error', 'n_generated_token', 'n_context_token', 'model', 'cost', 'complete_time', 'created_at')
+    list_display = ('question', 'answer', 'channel', 'token', 'user_id', 'is_error_occurred', 'error', 'num_of_input_tokens', 'num_of_output_tokens', 'model', 'cost', 'requested_at', 'multiquery_started_at', 'multiquery_ended_at', 'llm_request_started_at', 'llm_request_ended_at', 'llm_response_started_at', 'llm_response_ended_at', 'created_at')
     list_filter = ('channel', 'is_error_occurred', 'model')
-    search_fields = ('question', 'answer', 'channel', 'token', 'user_id', 'error', 'n_generated_token', 'n_context_token', 'model', 'cost', 'complete_time')
+    search_fields = ('question', 'answer', 'channel', 'token', 'user_id', 'error', 'num_of_input_tokens', 'model', 'cost')
     ordering = ('-created_at',)
